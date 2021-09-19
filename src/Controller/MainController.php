c782feeb4eb1bf06faf30e36f49ce704e9a49622
malick tkr <malick.tounkara.1@gmiail.com>
2021-09-19 10:34:58 +0100

    autopanes ready 2

---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/change-lang/{locale}", name="lang")
+     */
+    public function changeLocale($locale, Request $request)
+    {
+    
+        $locale = $request->attributes->get('locale');
+        
+        $request->getSession()->set('_locale', $locale);
+        
+        $request->setLocale($request->getSession()->get('_locale', $locale));    
+        
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
